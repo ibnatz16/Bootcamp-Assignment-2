d90e14a7e78097cff893e67c49ab2d644cf2185e
ibnatz16 <34073539+ibnatz16@users.noreply.github.com>
2019-09-13 13:34:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = { // also submitted on canvas (this was put into the repository for convenience of grading
     db: {
       uri: 'mongodb+srv://guest:8VwK8n23iCyGasER@cen3031-course-hiulj.mongodb.net/Bootcamp2', //the URI of my mongo database.
     }
