926d57de40e63a6adf08cfb7409e6b2c7a9d957e
ibnatz16 <34073539+ibnatz16@users.noreply.github.com>
2019-09-13 13:33:29 -0400

    Update JSONtoMongo.js

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -8,16 +8,13 @@ var fs = require('fs'),
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
     config = require('./config'),
-    listingData;
+    listingData; //global variable used for parsing in readFile
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
 
 mongoose.connect('mongodb+srv://guest:8VwK8n23iCyGasER@cen3031-course-hiulj.mongodb.net/Bootcamp2');
-
-/*var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));*/
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -29,21 +26,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));*/
 fs.readFile('listings.json', 'utf8', function(err,schema){
   if(err) throw err;
   listingData = JSON.parse(schema);
-  //console.log(listingData);
-  /*var modelSchema = mongoose.model('entries', schema)
-  modelSchema.forEach(function(modelSchema){
-    console.log(modelSchema);
-  });*/
-  listingData.entries.forEach(function(element){
+  listingData.entries.forEach(function(element){ //forEach function used to look through each listing 
     new Listing(element).save(function(err){
       if(err) throw err;
-      //console.log('Document saved');
     });
   });
 
 });
-
-
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
